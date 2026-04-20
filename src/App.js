16b1c881ae9356ff735cf6cb32268de62e7dc59a
vishwa-kusambi <vishwanath.kusambi@lmsathena.com>
2026-04-20 14:49:28 +0530

    version 2 test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Hello ECS 🚀</h1>
-      <h2>Version 1</h2>
+      <h1>Pajji Chutya 🚀</h1>
+      <h2>Version 10</h2>
     </div>
   );
 }
