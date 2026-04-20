2a8bf836a366674d3a75e23657491d090a0ce53b
vishwa-kusambi <vishwanath.kusambi@lmsathena.com>
2026-04-20 13:43:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello ECS 🚀</h1>
+      <h2>Version 1</h2>
     </div>
   );
 }
